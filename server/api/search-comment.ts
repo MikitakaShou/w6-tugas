7fd1b5b11419f23423d286af91f7c22dc20f52da
Sammy Ghoutan Sitompul <sammy.ghoutan123@gmail.com>
2024-11-08 20:49:48 +0700

    Update search-comment.ts
    update sql injection

--- server/api/search-comment.ts
+++ server/api/search-comment.ts
@@ -12,8 +12,9 @@ export default defineEventHandler(async (event) => {
   }
 
   try {
-    const result = await prisma.$queryRawUnsafe(
-      `SELECT * FROM GuestbookEntry WHERE comment LIKE '%${search}%'`
+    const result = await prisma.$queryRaw(
+      `SELECT * FROM GuestbookEntry WHERE comment LIKE '%' || $1 || '%'`,
+      search
     );
 
     return result;
